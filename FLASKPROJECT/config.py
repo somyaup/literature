abf96c02a9fff707675102b16c8a61a1d5e0f2aa
Somya Upadhyay <39206850+somyaup@users.noreply.github.com>
2022-07-20 18:40:10 +0530

    Update config.py

--- FLASKPROJECT/config.py
+++ FLASKPROJECT/config.py
@@ -4,7 +4,7 @@ from flaskext.mysql import MySQL
 mysql = MySQL()
 
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'Smlplz*288'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
 app.config['MYSQL_DATABASE_DB'] = 'literature'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
